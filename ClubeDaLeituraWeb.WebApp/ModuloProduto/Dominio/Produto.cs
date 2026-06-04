0b9950467ae017653dc2d2db632628febbeac7c6
Davi Andrade <contatodaviandrade20@gmail.com>
2026-06-04 15:11:38 -0300

    Criado base da Classe Produto

--- ClubeDaLeituraWeb.WebApp/ModuloProduto/Dominio/Produto.cs
+++ ClubeDaLeituraWeb.WebApp/ModuloProduto/Dominio/Produto.cs
@@ -1,16 +1,48 @@
 using ClubeDaLeituraWeb.WebApp.Compartilhado.Dominio;
-
-namespace ClubeDaLeituraWeb.WebApp.ModuloProduto.Dominio;
+using ClubeDaLeituraWeb.WebApp.ModuloCategoria.Dominio;
 
 public class Produto : EntidadeBase<Produto>
 {
-    public override void Atualizar(Produto entidadeAtualizada)
+    public string Nome { get; private set; }
+    public string UnidadeMedida { get; private set; }
+    public decimal PrecoAproximado { get; private set; }
+    public Categoria Categoria { get; private set; }
+
+    public Produto(
+        string nome,
+        string unidadeMedida,
+        decimal precoAproximado,
+        Categoria categoria
+    )
     {
-        throw new NotImplementedException();
+        Nome = nome;
+        UnidadeMedida = unidadeMedida;
+        PrecoAproximado = precoAproximado;
+        Categoria = categoria;
     }
 
     public override List<string> Validar()
     {
-        throw new NotImplementedException();
+        List<string> erros = new List<string>();
+
+        if (Nome.Length < 2 || Nome.Length > 100)
+            erros.Add("O campo \"Nome\" deve conter entre 2 e 100 caracteres.");
+
+        if (string.IsNullOrWhiteSpace(UnidadeMedida))
+            erros.Add("O campo \"Unidade de Medida\" deve ser preenchido.");
+
+        if (PrecoAproximado == 0)
+            erros.Add("O campo \"Preço Aproximado\" deve ser preenchido.");
+
+        return erros;
+    }
+
+    public override void Atualizar(Produto entidadeAtualizada)
+    {
+
+        Nome = entidadeAtualizada.Nome;
+        UnidadeMedida = entidadeAtualizada.UnidadeMedida;
+        PrecoAproximado = entidadeAtualizada.PrecoAproximado;
+        Categoria = entidadeAtualizada.Categoria;
     }
 }
